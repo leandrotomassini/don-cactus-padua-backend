8e576f9b4adfe86648948b9405624b4949be2998
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-07-22 14:55:18 +0000

    Broadcast con sockets

--- models/server.js
+++ models/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const fileUpload = require('express-fileupload');
+
 
 const { dbConnection } = require('../database/config');
+const { socketController } = require('../sockets/controller');
 
 class Server {
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
 
         this.paths = {
             auth: '/api/auth',
@@ -26,6 +29,11 @@ class Server {
 
         // Rutas de mi aplicación
         this.routes();
+
+        // Sockets
+        this.sockets();
+
+
     }
 
     async conectarDB() {
@@ -44,13 +52,6 @@ class Server {
         // Directorio Público
         this.app.use(express.static('public'));
 
-        // Fileupload - Carga de archivos
-        this.app.use(fileUpload({
-            useTempFiles: true,
-            tempFileDir: '/tmp/',
-            createParentPath: true
-        }));
-
     }
 
     routes() {
@@ -60,8 +61,15 @@ class Server {
         this.app.use(this.paths.productos, require('../routes/productos'));
     }
 
+    sockets() {
+
+        this.io.on('connection', socketController);
+
+
+    }
+
     listen() {
-        this.app.listen(this.port, () => {
+        this.server.listen(this.port, () => {
             console.log('Servidor corriendo en puerto', this.port);
         });
     }
